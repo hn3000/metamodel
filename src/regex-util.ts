00e25039c2f58d93f9f2765609a45cad80a7e02f
Harald Niesche <harald@niesche.de>
2018-10-21 16:42:53 +0200

    updated dependencies, bump version to 1.5.0

--- src/regex-util.ts
+++ src/regex-util.ts
@@ -3,24 +3,14 @@ export const SimpleReRE = /^\^\[((?:\\\[|\\\]|[^\[\]])+)\]([+*])\$$/;
 
 export function isInvertable(re:string|RegExp) {
 
-  let pattern:string;
-  switch (typeof re) {
-    case 'string': pattern = re as string; break;
-    case 'object': pattern = (re as RegExp).source; break;
-    default: pattern = null != re ? re.toString() : null; break
-  }
+  let pattern = reSource(re);
 
   return SimpleReRE.test(pattern);
 }
 
 export function invertedRE(re:string|RegExp):string {
 
-  let pattern:string;
-  switch (typeof re) {
-    case 'string': pattern = re as string; break;
-    case 'object': pattern = (re as RegExp).source; break;
-    default: pattern = null != re ? re.toString() : null; break
-  }
+  let pattern = reSource(re);
 
   let simpleReMatch = SimpleReRE.exec(pattern);
   if (simpleReMatch) {
@@ -34,4 +24,14 @@ export function invertedRE(re:string|RegExp):string {
   }
 
   return null;
+}
+
+function reSource(re: string|RegExp) {
+  let pattern: string;
+  switch (typeof re) {
+    case 'string': pattern = re as string; break;
+    case 'object': pattern = (re as RegExp).source; break;
+    default: pattern = (null != re) ? (re as any).toString() : null; break
+  }
+  return pattern;
 }
